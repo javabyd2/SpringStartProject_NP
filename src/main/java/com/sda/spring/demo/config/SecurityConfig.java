14e913ffe64d215bd7bef385a18eb8bf0de9863c
Natalia Palimaka <npalimaka@gmail.com>
2018-05-27 15:49:02 +0200

    update

--- src/main/java/com/sda/spring/demo/config/SecurityConfig.java
+++ src/main/java/com/sda/spring/demo/config/SecurityConfig.java
@@ -1,23 +1,47 @@
 package com.sda.spring.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
-    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder){
-        
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Value("${spring.query.users-query}")
+    private String usersQuery;
+    @Value("${spring.query.roles-query}")
+    private String rolesQuery;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManager) throws Exception {
+
+        authenticationManager.jdbcAuthentication()
+                .usersByUsernameQuery(usersQuery)
+                .authoritiesByUsernameQuery(rolesQuery)
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.authorizeRequests()
                 .antMatchers("/").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/register").permitAll()
                 .antMatchers("/web/roleform").hasAuthority("admin")
                 .anyRequest()
                 .authenticated().and().formLogin().loginPage("/login")
